86e1a30ae8cbc16382e6a3e1de25d825df9e28d5
Mohamed Ashraf <mohamedashrafemara19@gmail.com>
2023-11-23 10:57:55 +0200

    initial commit

---
+++ C-programming/lesson3/EX1.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : EX1_print a sentence
+ Author      : Mohamed Ashraf Emara
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+
+#include "stdio.h"
+
+
+void main()
+{
+    printf("C programming");
+}
